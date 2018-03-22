98fdbce27dc3bcbce9ccc1ff30bfa33ac9a72059
nucasspro <hailongdang.hailong@gmail.com>
2018-03-22 16:38:35 +0700

    Create project structure
    + Create project structure
    
    + Add Entity Framework into NCShop.Data, NCShop.Model, NCShop.WebApplication
    
    + Add reference:
    ++ NCShop.WebApplication add: NCShop.Common, NCShop.Data, NCShop.Model, NCShop.Service
    ++ NCShop.UnitTest add: NCShop.Common, NCShop.Data, NCShop.Model, NCShop.Service, NCShop.WebApplication
    ++ NCShop.Service add: NCShop.Common, NCShop.Data, NCShop.Model
    ++ NCShop.Data add: NCShop.Common
    ++ NCShop.Common: none
    ++ NCShop.Model: none

---
+++ NCShop/NCShop.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NCShop.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NCShop.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2913077a-e93d-4fd8-8319-60981464ea70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
